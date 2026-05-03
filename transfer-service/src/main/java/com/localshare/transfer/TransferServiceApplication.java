1bb878b3757e6ad8bfda8c281eda339847904532
suyash5053 <suyashsrivastava5053@gmail.com>
2026-05-03 20:43:01 +0530

    feat: adding Configuration properties, so that application yaml can be referenced

--- transfer-service/src/main/java/com/localshare/transfer/TransferServiceApplication.java
+++ transfer-service/src/main/java/com/localshare/transfer/TransferServiceApplication.java
@@ -1,9 +1,12 @@
 package com.localshare.transfer;
 
+import com.localshare.transfer.config.TransferProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(TransferProperties.class)
 public class TransferServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(TransferServiceApplication.class, args);
