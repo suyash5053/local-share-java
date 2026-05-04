47fb8d01a56b70f9006d92f22c3d3651ada9ac75
suyash5053 <suyashsrivastava5053@gmail.com>
2026-05-04 17:53:06 +0530

    feat: adding dependency and paths to various service in application yml

--- gateway-service/build.gradle.kts
+++ gateway-service/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     implementation(project(":common"))
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
